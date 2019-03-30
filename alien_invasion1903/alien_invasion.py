dc7ab80e832d9b28313f00c6170bb7aafbd2fdef
wangze <w18829898295@163.com>
2019-03-30 10:29:13 +0800

    创建设置类1903

--- alien_invasion1903/alien_invasion.py
+++ alien_invasion1903/alien_invasion.py
@@ -1,14 +1,16 @@
-# -*- coding:utf-8 -*-
+# -*- coding:GBK -*-
 
 import sys
 import pygame
+from settings import Settings							#导入Settings类
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
 	pygame.init()										#初始化游戏
-	screen = pygame.display.set_mode((1200, 800))		#创建一个窗口对象
+	ai_settings = Settings()							#创建一个Settings实例,并存储到变量ai_settings中
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))		#创建一个窗口对象
 	pygame.display.set_caption("Alien Invasion")		#窗口命名
-	bg_color = (230, 230, 230)							#存储颜色
 	
 	#开始游戏的主循环
 	while True:
@@ -19,7 +21,7 @@ def run_game():
 				sys.exit()								#退出
 				
 		#让最近绘制的屏幕可见
-		screen.fill(bg_color)							#使用颜色填充屏幕
+		screen.fill(ai_settings.bg_color)				#使用颜色填充屏幕
 		pygame.display.flip()							#刷新最近绘制的屏幕
 		
 		
