0f68a426430690654fcdea8447b8c0df25188eef
wangze <w18829898295@163.com>
2019-04-01 10:38:41 +0800

    绘制飞船1904

--- alien_invasion1903/alien_invasion.py
+++ alien_invasion1903/alien_invasion.py
@@ -3,6 +3,7 @@
 import sys
 import pygame
 from settings import Settings							#导入Settings类
+from ship import Ship
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
@@ -11,6 +12,8 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))		#创建一个窗口对象
 	pygame.display.set_caption("Alien Invasion")		#窗口命名
+	#创建一艘飞船
+	ship = Ship(screen)
 	
 	#开始游戏的主循环
 	while True:
@@ -22,6 +25,7 @@ def run_game():
 				
 		#让最近绘制的屏幕可见
 		screen.fill(ai_settings.bg_color)				#使用颜色填充屏幕
+		ship.blitme()									#绘制飞船,确保飞船在背景前
 		pygame.display.flip()							#刷新最近绘制的屏幕
 		
 		
---
+++ alien_invasion1903/ship.py
@@ -0,0 +1,21 @@
+# -*- coding:GBK -*-
+
+import pygame
+
+class Ship():
+	def __init__(self, screen):
+		"""初始化飞船并设置其初始位置"""
+		self.screen = screen
+		
+		#加载飞船图像并获取其外接矩形
+		self.image = pygame.image.load('images/ship.bmp')	#加载并存储飞船
+		self.rect = self.image.get_rect()					#获取surface属性rect(飞船矩形)
+		self.screen_rect = screen.get_rect()				#获取屏幕矩形
+		
+		#将每艘飞船放在屏幕底部中央
+		self.rect.centerx = self.screen_rect.centerx		#屏幕中央x值赋给飞船中央x
+		self.rect.bottom = self.screen_rect.bottom			#屏幕底部y值赋给飞船底部y
+		
+	def blitme(self):
+		"""在指定位置绘制飞船"""
+		self.screen.blit(self.image, self.rect)				#根据self.rect指定位置将飞船绘制在屏幕上
