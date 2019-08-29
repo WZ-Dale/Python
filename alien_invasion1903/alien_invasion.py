85dca9839105497d2a455893762c278bb4c126ca
wangze <w18829898295@163.com>
2019-08-29 19:58:44 +0800

    外星人向下移动

--- alien_invasion1903/alien_invasion.py
+++ alien_invasion1903/alien_invasion.py
@@ -29,8 +29,9 @@ def run_game():
 		ship.update()
 		#更新子弹位置
 		gf.update_bullets(bullets)
+		#更新外星人位置
+		gf.update_aliens(ai_settings, aliens)
 		#让最近绘制的屏幕可见
 		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-		
-		
+			
 run_game()
