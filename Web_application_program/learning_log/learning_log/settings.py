9593f1d619f1dba57ece2c7faa5c1f62c19b85fe
wangze <w18829898295@163.com>
2019-09-11 20:38:29 +0800

    页面样式

--- Web_application_program/learning_log/learning_log/settings.py
+++ Web_application_program/learning_log/learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #第三方应用程序
+    'bootstrap3',
+
     #我的应用程序
     'learning_logs',
     'users',
@@ -123,3 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_URL = '/users/login/'
+
+#django-bootstrap3的设置
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
