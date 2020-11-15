d0b0d877e28dd83f5088ba17a4f93c748ff1bcd3
WZ-Dale <w18829898295@163.com>
2020-11-15 12:22:08 +0800

    实操案例五

--- ProjectsMSB/case.py
+++ ProjectsMSB/case.py
@@ -62,5 +62,117 @@ print("BMI指数是："+str(bmi))
 print("BMI指数是："+"{:0.2f}".format(bmi))
 print("BMI指数是：{:0.2f}".format(bmi))
 
+"""实操案例3"""
+# 1.进制转换
+def fun():
+    num=int(input("请输入一个十进制的整数:"))
+    print(num,"的二进制数为:",bin(num))    # 个数可变的位置参数
+    print(str(num)+"的二进制数为:"+bin(num))    # +的左右均为str
+    print("%s的二进制数为:%s"%(num,bin(num)))    # 格式化输出
+    print("{0}的二进制数为:{1}".format(num,bin(num)))
+    print(f"{num}的二进制数为:{bin(num)}")
+    print("-----------------------------------")
+    print(f"{num}的八进制数为:{oct(num)}")
+    print(f"{num}的十六进制数为:{hex(num)}")
+while True:
+    try:
+        fun()
+    except:
+        print("只可以输入整数!")
+    else:
+        break
+# 2.手机充值
+money=8
+print(f"手机余额为:\033[0;35m{money}元\033[m")
+money+=int(input("充值:"))
+print(f"当前余额为:\033[0;35m{money}元\033[m")
+# 3.能量消耗
+calorie=28*int(input("当天步数:"))
+print(f"消耗卡路里:{calorie},即{calorie/1000}千卡")
+# 4.预测子女身高
+father_height=float(input("父亲的身高:"))
+monther_height=float(input("母亲的身高:"))
+son_height=(father_height+monther_height)*0.54
+print("子女的身高预测值:{:.2f}cm".format(son_height))
+
+"""实操案例4"""
+# 1.支付密码验证
+pwd=input("支付宝支付密码：")
+if pwd.isdigit():
+    print("合法")
+else:
+    print("不合法")
+print("- - - - - - - - - - - - - - -")
+print("合法" if pwd.isdigit() else "不合法")
+# 2.模拟账号登录
+qq=input("请输入账号：")
+pwd=input("请输入密码：")
+if qq=="123" and pwd=="456":
+    print("成功")
+else:
+    print("失败")
+# 3.价格竞猜
+import random
+price=random.randint(1000,1500)
+print("价格区间为：1000-1500")
+while True:
+    guess=int(input("输入竞猜："))
+    if guess>price:
+        print("大了")
+    elif guess<price:
+        print("小了")
+    else:
+        print("正确！！")
+        break
+# 4.查看星座运势
+d={"狮子座":"双子座","双子座":"狮子座"}
+star=input("请输入星座：")
+print(f"匹配星座为：{d.get(star)}")    # 使用get方法，即使获取不到也不会报错
+
+"""实操案例5"""
+# 1.循环输出26个字母对应的ASCII码值
+x=97    # a的ASCII码值
+for _ in range(1,27):
+    print(chr(x),'--->',x)
+    x+=1
+print("--------------------------")
+x=97
+while x < 123:
+    print(chr(x),"--->",x)
+    x+=1
+# 2.模拟用户登录
+for i in range(1,4):
+    qq = input("请输入账号：")
+    pwd = input("请输入密码：")
+    if qq == "123" and pwd == "456":
+        print("成功")
+        break
+    else:
+        print("失败")
+        if i < 3:
+            print(f"还有{3-i}次机会")
+else:
+    print("三次均错误，账号锁定")
+# 3.猜数
+import random
+rand=random.randint(1,100)
+for i in range(1,11):
+    num=int(input("范围1-100，请猜:"))
+    if num < rand:
+        print("小了")
+    elif num > rand:
+        print("大了")
+    else:
+        print("恭喜你猜对了")
+        print(f"一共猜了{i}次")
+        break
+# print(f"一共猜了{i}次")
+# 4.计算100-999之间的水仙花数
+import math
+for i in range(100, 1000):
+    if math.pow((i%10),3)+math.pow((i//10%10),3)+math.pow((i//100),3)==i:
+        print(i)
+
+
 
 
